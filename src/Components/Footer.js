641f7380ac452f7c7084f309e05f9b86956601e3
peetzaecksdee <peetzaecksdee@hotmail.com>
2024-04-21 13:17:11 +0700

    Added Files

---
+++ src/Components/Footer.js
@@ -0,0 +1,28 @@
+import "./Footer.css";
+import pizza from '../assets/images/pizza.jpg';
+
+function loadFooter() {
+	const footer = document.createElement("footer");
+
+	const text = document.createElement("i");
+	text.textContent = "Another day, Another project";
+	text.classList.add("credit");
+
+	const githubPage = document.createElement("a");
+	githubPage.href = "https://github.com/peetzaecksdee";
+	githubPage.classList.add("credit-a");
+  githubPage.target = "_blank";
+
+	const githubImage = new Image();
+	githubImage.src = pizza;
+	githubImage.alt = "my profile";
+
+	githubPage.appendChild(githubImage);
+
+	footer.appendChild(text);
+	footer.appendChild(githubPage);
+
+	return footer;
+}
+
+export default loadFooter;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import './style.css'
+import loadFooter from "./Components/Footer";
+
+function init() {
+  const content = document.querySelector('#content');
+  content.appendChild(loadFooter());
+}
+
+init();
